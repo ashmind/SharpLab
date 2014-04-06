3555ca01840f50fbdcebd87581eaf4e31bc932f8
Andrey Shchekin <ashmind@gmail.com>
2014-04-07 11:40:26 +1200

    Added shareable URLs.

--- Web/StaticHtml/app.js
+++ Web/StaticHtml/app.js
@@ -2,35 +2,67 @@
 
 $(function() {
   'use strict';
-  var storageKey = '__tryroslyn__code__';
-  var editor = CodeMirror.fromTextArea($('#code textarea')[0], {
-    mode:        'text/x-csharp',
-    lineNumbers: true,
-    indentUnit:  4
-  });
+
+  var editor;
+  var decompiled;
+
+  var lastHash;
+
+  start();
+
+  function start() {
+    editor = CodeMirror.fromTextArea($('#code textarea')[0], {
+      mode:        'text/x-csharp',
+      lineNumbers: true,
+      indentUnit:  4
+    });
     
-  var decompiled = CodeMirror.fromTextArea($('#decompiled textarea')[0], {
-    mode:     'text/x-csharp',
-    readOnly: true
-  });
-  
-  load(editor);
-  editor.on('change', $.debounce(600, function() {
-    save(editor);
-    update();
-  }));
-  update();
-  
-  function save() {
-    localStorage.setItem(storageKey, editor.getValue());
+    decompiled = CodeMirror.fromTextArea($('#decompiled textarea')[0], {
+      mode:     'text/x-csharp',
+      readOnly: true
+    });
+
+    if (!tryLoadUrl())
+      editor.setValue(getDefaultValue());
+
+    var throttledSaveUrl = $.debounce(100, saveUrl);
+    var throttledUpdateFromServer = $.debounce(600, updateFromServer);
+    editor.on('change', function() {
+      throttledSaveUrl();
+      throttledUpdateFromServer();
+    });
+    saveUrl();
+    updateFromServer();
+    
+    $(window).hashchange(function() { tryLoadUrl(); });
   }
   
-  function load() {
-    var value = localStorage.getItem(storageKey);
-    if (value === undefined || value === null)
-        value = getDefaultValue();
+  function saveUrl() {
+    var hash = LZString.compressToBase64(editor.getValue());
+    lastHash = hash;
+    window.location.hash = hash;
+  }
+  
+  function tryLoadUrl() {
+    var hash = window.location.hash;
+    if (!hash)
+      return false;
+
+    hash = hash.replace(/^#/, '');
+    if (!hash || hash === lastHash)
+      return false;
+
+    var value;
+    lastHash = hash;
+    try {
+      value = LZString.decompressFromBase64(hash);
+    }
+    catch (e) {
+      return false;
+    }
 
     editor.setValue(value);
+    return true;
   }
   
   function getDefaultValue() {
@@ -43,7 +75,7 @@ $(function() {
     return value.replace(new RegExp(indent, 'g'), '');
   }
     
-  function update() {
+  function updateFromServer() {
     var code = editor.getValue();
     $.ajax("api/compilation", {
       method: 'POST',
