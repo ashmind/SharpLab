3d5364ac9fd16c8633110fa66731c0ea427bf489
Andrey Shchekin <ashmind@gmail.com>
2014-05-15 02:16:23 +1200

    Container fix.

--- Core/CoreModule.cs
+++ Core/CoreModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using AshMind.IO.Abstractions;
 using AshMind.IO.Abstractions.Adapters;
 using Autofac;
 using TryRoslyn.Core.Processing;
@@ -13,6 +14,10 @@ namespace TryRoslyn.Core {
         protected override void Load(ContainerBuilder builder) {
             var configurationPath = Path.GetDirectoryName(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
             var binariesRoot = Path.Combine(configurationPath, ConfigurationManager.AppSettings["BinariesRoot"]);
+
+            builder.RegisterType<FileSystem>()
+                   .As<IFileSystem>()
+                   .SingleInstance();
 
             builder.Register(c => new BranchProvider(new DirectoryInfoAdapter(new DirectoryInfo(binariesRoot))))
                    .As<IBranchProvider>()
