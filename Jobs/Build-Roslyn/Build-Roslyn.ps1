9eaaed158e2a036c17975f5499c79a0c77a08fbc
Andrey Shchekin <ashmind@gmail.com>
2014-11-01 20:38:44 +1300

    Updated build to notify on failure.

--- Jobs/Build-Roslyn/Build-Roslyn.ps1
+++ Jobs/Build-Roslyn/Build-Roslyn.ps1
@@ -1,10 +1,34 @@
 Set-StrictMode -Version 2.0
 $ErrorActionPreference = 'Stop'
-# Note: Write-Host, Write-Error and Write-Warning do not function properly in Azure
 
+# Note: Write-Host, Write-Error and Write-Warning do not function properly in Azure
 $MSBuild = ${env:ProgramFiles(x86)} + '\MSBuild\12.0\bin\MSBuild.exe'
+$WebHookUrl = ${env:WEBJOB_BUILD_WEBHOOK_URL}
 
 # Functions ------
+function Send-Notification(
+    [Parameter(Mandatory=$true)][string]$title,
+    [Parameter(Mandatory=$true)][string]$message,
+    [string]$logPath
+) {
+    if ($WebHookUrl -eq $null) {
+        return
+    }
+   
+    "Sending notification to webhook at $WebHookUrl" | Out-Default
+    $json = @{
+      title="Build-Roslyn.ps1: $title";
+      message=$message;
+      log=[IO.File]::ReadAllText($logPath);
+    } | ConvertTo-Json
+    
+    Invoke-RestMethod $WebHookUrl `
+                      -Method Post `
+                      -Body $json `
+                      -ContentType 'application/json' `
+                      -ErrorAction Continue | Out-Null
+}
+
 function Sync-Branch($directory, $branch) {
     Write-Output "Syncing $directory"
     if (Test-Path $directory) {
@@ -28,72 +52,46 @@ function Build-Branch($directory, $branch) {
     Write-Output "Building $directory"
 
     $buildLogPath = "$sourcesRoot\$fsName.build.log"
+    if (Test-Path $buildLogPath) {
+        Remove-Item $buildLogPath
+    }
+    
     Copy-Item NuGet.Roslyn.config "$directory\NuGet.config" -Force
-
-    Write-Output "  BuildAndTest.proj"
-    &$MSBuild "$directory\BuildAndTest.proj" /target:RestorePackages > "$buildLogPath"
-    if ($LastExitCode -ne 0) {
-        Write-Output "  [WARNING] Build failed, see $buildLogPath"
-        return
+    
+    function Build-Project(
+        [Parameter(Mandatory=$true)][string[]] $candidateProjectPaths,
+        [string] $msbuildArgs
+    ) {    
+        $projectPath = @($candidateProjectPaths | ? { Test-Path "$directory\$_" })[0];        
+        "  $projectPath" | Out-Default
+        
+        $projectPath = "$directory\$projectPath"
+        Invoke-Expression $("&`"$MSBuild`" `"$projectPath`" $msbuildArgs >> `"$buildLogPath`"")
+        if ($LastExitCode -ne 0) {
+            "  [WARNING] Build failed, see $buildLogPath" | Out-Default
+            Send-Notification -Title "'$branch' build failed" -Message "See attached log for details" -LogPath $buildLogPath
+            return $false
+        }
+        return $true    
     }
-
-    $csProjectPath = @(@(
-      "Src\Compilers\CSharp\Source\CSharpCodeAnalysis.csproj";
-      "Src\Compilers\CSharp\Desktop\CSharpCodeAnalysis.Desktop.csproj"
-    ) | ? { Test-Path "$directory\$_" })[0];
-
-    Write-Output "  $csProjectPath"
-    &$MSBuild "$directory\$csProjectPath" `
-        /p:RestorePackages=false `
-        /p:DelaySign=false `
-        /p:SignAssembly=false `
-        /p:SolutionDir="$directory\Src" `
-        >> "$buildLogPath"
-
-    if ($LastExitCode -ne 0) {
-        Write-Output "  [WARNING] Build failed, see $buildLogPath"
-        return
-    }
-
-    $vbSyntaxGeneratorProjectPath = "Src\Tools\Source\CompilerGeneratorTools\Source\VisualBasicSyntaxGenerator\VisualBasicSyntaxGenerator.vbproj"
-    Write-Output "  $vbSyntaxGeneratorProjectPath"
-    &$MSBuild "$directory\$vbSyntaxGeneratorProjectPath" `
-        /p:RestorePackages=false `
-        /p:DelaySign=false `
-        /p:SignAssembly=false `
-        /p:Configuration=Debug `
-        /p:SolutionDir="$directory\Src" `
-        >> "$buildLogPath"
-
-    if ($LastExitCode -ne 0) {
-        Write-Output "  [WARNING] Build failed, see $buildLogPath"
-        return
-    }
-
-    $vbProjectPath = @(@(
-      "Src\Compilers\VisualBasic\Source\BasicCodeAnalysis.vbproj";
-      "Src\Compilers\VisualBasic\Desktop\BasicCodeAnalysis.Desktop.vbproj"
-    ) | ? { Test-Path "$directory\$_" })[0];
-    Write-Output "  $vbProjectPath"
-    &$MSBuild "$directory\$vbProjectPath" `
-        /p:IldasmPath="$toolsRoot\ildasm.exe" `
-        /p:RestorePackages=false `
-        /p:DelaySign=false `
-        /p:SignAssembly=false `
-        /p:Configuration=Debug `
-        /p:SolutionDir="$directory\Src" `
-        >> "$buildLogPath"
-
-    if ($LastExitCode -ne 0) {
-        Write-Output "  [WARNING] Build failed, see $buildLogPath"
-        return
-    }
+            
+    if (!(Build-Project BuildAndTest.proj "/target:RestorePackages")) { return }
+    
+    $standardArgs = "/p:RestorePackages=false /p:Configuration=Debug /p:DelaySign=false /p:SignAssembly=false /p:SolutionDir=`"$directory\Src`""
+    $csCandidates = @("Src\Compilers\CSharp\Source\CSharpCodeAnalysis.csproj", "Src\Compilers\CSharp\Desktop\CSharpCodeAnalysis.Desktop.csproj");
+    if (!(Build-Project $csCandidates $standardArgs)) { return }
+    
+    $vbSyntaxGenerator = "Src\Tools\Source\CompilerGeneratorTools\Source\VisualBasicSyntaxGenerator\VisualBasicSyntaxGenerator.vbproj";
+    if (!(Build-Project $vbSyntaxGenerator $standardArgs)) { return }
+    
+    $vbCandidates = @("Src\Compilers\VisualBasic\Source\BasicCodeAnalysis.vbproj", "Src\Compilers\VisualBasic\Desktop\BasicCodeAnalysis.Desktop.vbproj");
+    if (!(Build-Project $vbCandidates "$standardArgs /p:IldasmPath=`"$toolsRoot\ildasm.exe`"")) { return }
 
     $binariesDirectory = "$binariesRoot\" + $fsName
     robocopy "$directory\Binaries\Debug" $binariesDirectory /MIR /XF "LastCommit.txt"
     Copy-Item "$sourcesRoot\$fsName.lastcommit.txt" -Destination "$binariesDirectory\LastCommit.txt"
     Remove-Item "$directory\NuGet.config"
-
+    
     Write-Output "  Build completed"
 }
 
@@ -107,10 +105,11 @@ try {
     Write-Output "Environment:"
     Write-Output "Current path: $(Get-Location)"
     Write-Output "WEBROOT_PATH: ${env:WEBROOT_PATH}"
-
+    Write-Output "WebHookUrl: $WebHookUrl"
+    
     $webRoot = Resolve-Path $env:WEBROOT_PATH
     Write-Output "   $webRoot"
-
+    
     $webRoot = Resolve-Path $env:WEBROOT_PATH
     $sourcesRoot = "$webRoot\..\!roslyn-sources"
     $toolsRoot = "$webRoot\..\!roslyn-build-tools"
@@ -122,7 +121,7 @@ try {
     }
     $sourcesRoot = Resolve-Path $sourcesRoot
     Write-Output "Sources Root: $sourcesRoot"
-
+    
     if (-not (Test-Path $toolsRoot)) {
         Throw "Path not found: $toolsRoot"
     }
@@ -153,16 +152,16 @@ try {
         Sync-Branch $directory $_
         Build-Branch $directory $_
     }
-
+    
     Remove-Item .git -Force -Recurse
     [IO.File]::SetLastWriteTime("$webRoot\web.config", [DateTime]::Now)
-
+    
     #Write-Output "Killing VBCSCompiler instances"
     #taskkill /IM VBCSCompiler.exe /F
 }
-catch {
-    $ErrorActionPreference = 'Continue'
+catch {    
     Write-Output "[ERROR] $_"
+    Send-Notification -Title "Critical failure" -Message $_
     Write-Output 'Returning exit code 1'
     exit 1
 }
