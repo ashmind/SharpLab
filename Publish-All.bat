9d4418bdcb687293123be389aec34ed7d2a67cdd
Andrey Shchekin <ashmind@gmail.com>
2016-01-16 23:46:01 +1300

    Attempt at Publish fix.

--- Publish-All.bat
+++ Publish-All.bat
@@ -1,2 +1,6 @@
 @echo off
-powershell "Set-ExecutionPolicy RemoteSigned Process; .\#build\Publish-All.ps1"
+set powershell=powershell
+set sysnative="%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe"
+if exist %sysnative% (set powershell=%sysnative%)
+
+%powershell% "Set-ExecutionPolicy RemoteSigned Process; .\#build\Publish-All.ps1"
