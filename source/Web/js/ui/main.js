feb7c79a74ca5eed7dd8e400c9f7da6c1dca2c79
Andrey Shchekin <ashmind@gmail.com>
2016-04-17 21:05:28 +1200

    Migrated mobile editor full-screening to the new UI.

--- source/Web/js/ui/main.js
+++ source/Web/js/ui/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import $ from 'jquery';
+import hooks from './hooks/registry';
 
-import './filters/app-date.js';
-import './filters/app-trim.js';
-import './app-codemirror.js';
-import './app-diagnostic.js';
+import './filters/app-date';
+import './filters/app-trim';
+import './components/app-codemirror';
+import './components/app-diagnostic';
+import './hooks/app-mobile-codemirror-fullscreen';
 
 export default function(model) {    
     return new Promise(function(resolve, reject) {
@@ -13,7 +15,12 @@ export default function(model) {
                 // jshint -W031
                 new Vue({
                     el: 'body',
-                    data: model
+                    data: model,
+                    ready: function() {
+                        for (let hook of hooks.ready) {
+                            hook(this);
+                        }
+                    }
                 });
             }
             catch (e) {
