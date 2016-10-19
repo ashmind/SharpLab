45b88f92cb3ac37f52b6d86c0fbe886c9cf5d357
Andrey Shchekin <ashmind@gmail.com>
2016-10-19 23:43:43 +1300

    Added favicons.

--- source/Web/gulpfile.js
+++ source/Web/gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var g = require('gulp-load-plugins')();
 var webpack = require('webpack-stream');
 var assign = require('object-assign');
+var pipe = require('multipipe');
 
 gulp.task('less', function () {
     return gulp
@@ -28,6 +29,22 @@ gulp.task('js', function () {
         .pipe(gulp.dest('wwwroot'));
 });
 
+gulp.task('favicon', function () {
+    return gulp
+        .src('./favicon.svg')
+        .pipe(g.mirror(
+          g.rename('favicon.svg'),
+          pipe(g.svg2png({ width:  16, height:  16 }), g.rename('favicon-16.png')),
+          pipe(g.svg2png({ width:  32, height:  32 }), g.rename('favicon-32.png')),
+          pipe(g.svg2png({ width:  64, height:  64 }), g.rename('favicon-64.png')),
+          pipe(g.svg2png({ width:  96, height:  96 }), g.rename('favicon-64.png')),
+          pipe(g.svg2png({ width: 128, height: 128 }), g.rename('favicon-128.png')),
+          pipe(g.svg2png({ width: 196, height: 196 }), g.rename('favicon-196.png')),
+          pipe(g.svg2png({ width: 256, height: 256 }), g.rename('favicon-256.png'))
+        ))
+        .pipe(gulp.dest('wwwroot/favicons'));
+});
+
 gulp.task('html', function () {
     return gulp
         .src('./index.html')
@@ -38,7 +55,8 @@ gulp.task('html', function () {
 gulp.task('watch', ['default'], function () {
     gulp.watch('less/**/*.less', ['less']);
     gulp.watch('js/**/*.js', ['js']);
+    gulp.watch('favicon.svg', ['favicon']);
     gulp.watch('index.html', ['html']);
 });
 
-gulp.task('default', ['less', 'js', 'html']);
+gulp.task('default', ['less', 'js', 'favicon', 'html']);
