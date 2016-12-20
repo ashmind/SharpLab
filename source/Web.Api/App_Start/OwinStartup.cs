3f5729ebe4f9d360693f9396fbe26e6b4472039e
Andrey Shchekin <ashmind@gmail.com>
2016-12-20 20:31:29 +1300

    wip-1

---
+++ source/Web.Api/App_Start/OwinStartup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using MirrorSharp;
+using MirrorSharp.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TryRoslyn.Web.Api.OwinStartup), nameof(TryRoslyn.Web.Api.OwinStartup.Configuration))]
+
+namespace TryRoslyn.Web.Api {
+    public class OwinStartup {
+        public void Configuration(IAppBuilder app) {
+            app.UseMirrorSharp(new MirrorSharpOptions {
+                SlowUpdateExtra = {
+                    PrepareAsync = (session, token) => {
+                        return null;
+                    }
+                }
+            });
+        }
+    }
+}
