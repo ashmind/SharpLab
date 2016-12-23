015bc995dae5d0d66d6cdcb6564f1b72f4a949ec
Andrey Shchekin <ashmind@gmail.com>
2016-12-23 13:21:20 +1300

    wip-2

--- source/Web.Api/App_Start/OwinStartup.cs
+++ source/Web.Api/App_Start/OwinStartup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Autofac;
+using Microsoft.Owin;
 using MirrorSharp;
+using MirrorSharp.Advanced;
 using MirrorSharp.Owin;
 using Owin;
 
@@ -8,13 +10,16 @@ using Owin;
 namespace TryRoslyn.Web.Api {
     public class OwinStartup {
         public void Configuration(IAppBuilder app) {
-            app.UseMirrorSharp(new MirrorSharpOptions {
-                SlowUpdateExtra = {
-                    PrepareAsync = (session, token) => {
-                        return null;
-                    }
-                }
-            });
+            RegisterMirrorSharp(app);
+        }
+
+        private static void RegisterMirrorSharp(IAppBuilder app, IContainer container) {
+
+            var options = new MirrorSharpOptions {
+                SlowUpdate = container.Resolve<ICustomSlowUpdate>()
+            };
+
+            app.UseMirrorSharp(options);
         }
     }
 }
---
+++ source/Web.Api/Integration/SlowUpdate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.IO;
+using MirrorSharp.Advanced;
+using TryRoslyn.Core;
+using TryRoslyn.Core.Decompilation;
+
+namespace TryRoslyn.Web.Api.Integration {
+    public class SlowUpdate : ICustomSlowUpdate {
+        private readonly IReadOnlyCollection<IDecompiler> _decompilers;
+        private readonly RecyclableMemoryStreamManager _memoryStreamManager;
+
+        public SlowUpdate(IReadOnlyCollection<IDecompiler> decompilers, RecyclableMemoryStreamManager memoryStreamManager) {
+            _decompilers = decompilers;
+            _memoryStreamManager = memoryStreamManager;
+        }
+
+        public async Task<object> PrepareAsync(IWorkSession session, CancellationToken cancellationToken) {
+            var decompiler = _decompilers.First(d => d.Language == LanguageIdentifier.CSharp);
+            var compilation = await session.Project.GetCompilationAsync(cancellationToken).ConfigureAwait(false);
+
+            using (var stream = _memoryStreamManager.GetStream()) {
+                var emitResult = compilation.Emit(stream);
+                if (!emitResult.Success)
+                    throw new NotImplementedException();
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var resultWriter = new StringWriter();
+                decompiler.Decompile(stream, resultWriter);
+                return resultWriter.ToString();
+            }
+        }
+
+        public Task WriteAsync(IFastJsonWriter writer, object prepared, CancellationToken cancellationToken) {
+            var decompiled = (string) prepared;
+            writer.
+        }
+    }
+}
