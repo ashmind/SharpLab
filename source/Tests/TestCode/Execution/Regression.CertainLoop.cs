7ce751a3cb9048b3848f1168864eea8650257fcf
Andrey Shchekin <ashmind@gmail.com>
2017-08-10 20:12:52 +1200

    Cosmetic test fix.

--- source/Tests/TestCode/Execution/Regression.CertainLoop.cs
+++ source/Tests/TestCode/Execution/Regression.CertainLoop.cs
@@ -1,9 +1,6 @@
-﻿public static class Program
-{
-    public static void Main()
-    {
-        for (var i = 0; i < 1; i++)
-        {
+﻿public static class Program {
+    public static void Main() {
+        for (var i = 0; i < 1; i++) {
             var x = 0;
             if (x == 1) return;
             x = 1;
