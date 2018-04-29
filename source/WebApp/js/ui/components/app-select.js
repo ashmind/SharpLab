4d135fde12de377c95e2ece8a89bdd8352a624ad
Andrey Shchekin <ashmind@gmail.com>
2018-04-29 21:24:27 +1200

    [gh-274] Updated all `<select>` components to `<app-select>`.

--- source/WebApp/js/ui/components/app-select.js
+++ source/WebApp/js/ui/components/app-select.js
@@ -7,7 +7,7 @@ Vue.component('app-select', {
 
     template: `<div class="select-wrapper">
       <select v-bind:value="value"
-              v-on:input="$emit('input', $event.target.value)">
+              v-on:change="$emit('input', $event.target.value)">
         <slot></slot>
       </select>
     </div>`.replace(/[\r\n]+\s*/g, '').replace(/\s{2,}/g, ' ')
