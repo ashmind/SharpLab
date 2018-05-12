c6bd88ba1a3aaf1500245883880206cc0627e4ec
Andrey Shchekin <ashmind@gmail.com>
2018-05-13 09:38:41 +1200

    [gh-258] Moved feature mapping from build to publish.

--- #build/Publish-All.ps1
+++ #build/Publish-All.ps1
@@ -6,10 +6,52 @@ Set-StrictMode -Version 2.0
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = "SilentlyContinue" # https://www.amido.com/powershell-win32-error-handle-invalid-0x6/
 
-# Write-Host, Write-Error and Write-Warning do not function properly in Azure
-# So this mostly uses Write-Output for now
+# Write-Host, Write-Error and Write-Warning didn't function properly in Azure, so this mostly used Write-Output
+# However new code can use other ones
+
+$LangugageFeatureMapUrl = 'https://raw.githubusercontent.com/dotnet/roslyn/master/docs/Language%20Feature%20Status.md'
+
 $PublishToIIS = Resolve-Path "$PSScriptRoot\Publish-ToIIS.ps1"
 $PublishToAzure = Resolve-Path "$PSScriptRoot\Publish-ToAzure.ps1"
+
+function ConvertTo-Hashtable([PSCustomObject] $object) {
+    $result = @{}
+    $object.PSObject.Properties | % { $result[$_.Name] = $_.Value }
+    return $result
+}
+
+function Get-RoslynBranchFeatureMap($artifactsRoot) {
+    $markdown = (Invoke-WebRequest $LangugageFeatureMapUrl -UseBasicParsing)
+    $languageVersions = [regex]::Matches($markdown, '#\s*(?<language>.+)\s*$\s*(?<table>(?:^\|.+$\s*)+)', 'Multiline')
+
+    $mapPath = "$artifactsRoot/RoslynFeatureMap.json"
+    $map = @{}
+    if (Test-Path $mapPath) {
+        $map = ConvertTo-Hashtable (ConvertFrom-Json (Get-Content $mapPath -Raw))
+    }
+    $languageVersions | % {
+        $language = $_.Groups['language'].Value
+        $table = $_.Groups['table'].Value
+        [regex]::Matches($table, '^\|(?<rawname>[^|]+)\|.+roslyn/tree/(?<branch>[A-Za-z\d\-/]+)', 'Multiline') | % {
+            $name = $_.Groups['rawname'].Value.Trim()
+            $branch = $_.Groups['branch'].Value
+            $url = ''
+            if ($name -match '\[([^\]]+)\]\(([^)]+)\)') {
+                $name = $matches[1]
+                $url = $matches[2]
+            }
+            
+            $map[$branch] = [PSCustomObject]@{
+                language = $language
+                name = $name
+                url = $url
+            }
+        }
+    } | Out-Null
+    
+    Set-Content $mapPath (ConvertTo-Json $map)
+    return $map
+}
 
 function Login-ToAzure($azureConfig) {
     $passwordKey = $env:TR_AZURE_PASSWORD_KEY
@@ -57,6 +99,9 @@ try {
     &"$PSScriptRoot\#tools\nuget" install ftpush -Pre -OutputDirectory "$sourceRoot\!tools"
     $ftpushExe = @(Get-Item "$sourceRoot\!tools\ftpush*\tools\ftpush.exe")[0].FullName
     Write-Output "  ftpush.exe:            $ftpushExe"
+
+    Write-Output "Getting Roslyn feature map..."
+    $roslynBranchFeatureMap = Get-RoslynBranchFeatureMap -ArtifactsRoot $roslynArtifactsRoot
 
     if ($azure) {
         $azureConfigPath = ".\!Azure.config.json"
@@ -123,13 +168,19 @@ try {
         }
 
         # Success!
-        $branchesJson += [ordered]@{
+        $branchJson = [ordered]@{
             id = $branchFsName -replace '^dotnet-',''
             name = $branchInfo.name
             group = $branchInfo.repository
             url = $url
+            feature = $roslynBranchFeatureMap[$branchInfo.name]
             commits = $branchInfo.commits
         }
+        if (!$branchJson.feature) {
+            $branchJson.Remove('feature')
+        }
+
+        $branchesJson += $branchJson
     }
 
     $branchesFileName = "!branches.json"
--- #build/Publish-ToIIS.ps1
+++ #build/Publish-ToIIS.ps1
@@ -6,7 +6,7 @@ param (
 Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop'
 
-Import-Module WebAdministration
+Import-Module IISAdministration
 
 function Register-HostsRecord([string] $hostName, [string] $ip) {
     $hostsPath = Join-Path ([System.Environment]::GetFolderPath('System')) "drivers\etc\hosts"
@@ -31,31 +31,36 @@ function Register-HostsRecord([string] $hostName, [string] $ip) {
 # So this mostly uses Write-Output for now
 Write-Output "Deploying to IIS, $siteName..."
 
+Start-IISCommitDelay
+$server = Get-IISServerManager
+
 $appPoolName = $siteName
 if ($appPoolName.Length -gt 64) {
     $parts = $appPoolName -split '\.',2
     $first = $parts[0].Substring(0, $parts[0].Length - ($appPoolName.Length - 64) - 3)
     $appPoolName = "$first(…).$($parts[1])"
 }
-$appPool = (Get-ChildItem 'IIS:\AppPools\' | ? { $_.Name -eq $appPoolName })
+$appPool = $server.ApplicationPools[$appPoolName]
 if (!$appPool) {
     Write-Output "  Creating app pool..."
-    $appPool = New-WebAppPool $appPoolName
+    $appPool = $server.ApplicationPools.Add($appPoolName)
 }
 else {
     Write-Output "  App pool found."
 }
 
-$website = (Get-Website | ? { $_.Name -eq $siteName })
+$website = $server.Sites[$siteName]
 if (!$website) {
     Write-Output "  Creating web site..."
-    $website = New-Website $siteName `
-        -HostHeader $siteName `
-        -PhysicalPath (Resolve-Path $sourcePath) `
-        -ApplicationPool $($appPool.Name)
+    $website = New-IISSite `
+        -Name $siteName `
+        -BindingInformation "*:80:$siteName" `
+        -PhysicalPath (Resolve-Path $sourcePath)
+    $website.Applications["/"].ApplicationPoolName = $appPoolName
 }
 else {
     Write-Output "  Web site found."
 }
+Stop-IISCommitDelay
 
 Register-HostsRecord -HostName $siteName -IP '127.0.0.1'
