56d5e288ebf8aec4bac4d540f2df9af8a2692f10
Andrey Shchekin <ashmind@gmail.com>
2018-08-12 17:55:49 +1200

    [gh-290] Improved no-explanations content.

--- source/WebApp/js/ui/components/app-explain-view.js
+++ source/WebApp/js/ui/components/app-explain-view.js
@@ -12,19 +12,5 @@ Vue.component('app-explain-view', {
             return /^[aeiou]/.test(string) ? 'an' : 'a';
         }
     },
-    template: `<div class="result-content explanations">
-        <small class="output-disclaimer">
-            This is a new feature — some things might be left unexplained.
-        </small>
-        <dl>
-            <template v-for="explanation in explanations">
-                <dt>
-                    <code>{{explanation.code.trim()}}</code> is {{a(explanation.name)}} <dfn>{{explanation.name}}</dfn>.
-                    <span class="explanation-doc-link">[<a v-bind:href="explanation.link" target="_blank">Docs</a>]</span>
-                </dt>
-                <dd v-html="markdown(explanation.text)" class="markdown">
-                </dd>
-            </template>
-        </dl>
-    </div>`.replace(/[\r\n]+\s*/g, '').replace(/\s{2,}/g, ' ')
+    template: '#app-explain-view'
 });
