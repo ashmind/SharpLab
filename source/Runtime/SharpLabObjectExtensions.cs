2febb29dbd2bca2d0d7900a12c6856f9cea8f45b
Alex Gravely <tcbskater@hotmail.com>
2018-10-11 11:17:54 -0400

    Return object from Dump/Inspect extension methods.

--- source/Runtime/SharpLabObjectExtensions.cs
+++ source/Runtime/SharpLabObjectExtensions.cs
@@ -5,14 +5,14 @@ using SharpLab.Runtime.Internal;
 public static class SharpLabObjectExtensions {
     // LinqPad/etc compatibility only
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public static void Dump<T>(this T value) {
-        value.Inspect(title: "Dump");
-    }
+    public static T Dump<T>(this T value) 
+        => value.Inspect(title: "Dump");
 
-    public static void Inspect<T>(this T value, string title = "Inspect") {
+    public static T Inspect<T>(this T value, string title = "Inspect") {
         var builder = new StringBuilder();
         ObjectAppender.Append(builder, value);
         var data = new SimpleInspectionResult(title, builder);
         Output.Write(data);
+        return value;
     }
 }
