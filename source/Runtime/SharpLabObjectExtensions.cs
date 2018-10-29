dde7e28f60ebc2282f6ea55b6ce42188183ea69a
Alex Gravely <tcbskater@hotmail.com>
2018-10-29 14:03:07 -0400

    Return value in Dump ext. method.

--- source/Runtime/SharpLabObjectExtensions.cs
+++ source/Runtime/SharpLabObjectExtensions.cs
@@ -5,8 +5,10 @@ using SharpLab.Runtime.Internal;
 public static class SharpLabObjectExtensions {
     // LinqPad/etc compatibility only
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public static T Dump<T>(this T value) 
-        => value.Inspect(title: "Dump");
+    public static T Dump<T>(this T value) {
+        value.Inspect(title: "Dump");
+        return value;
+    }
 
     public static void Inspect<T>(this T value, string title = "Inspect") {
         var builder = new StringBuilder();
