d59d2962b382f3bb196c4a27dd0d4c6dab6791a8
Andrey Shchekin <ashmind@gmail.com>
2019-04-26 00:43:37 +1200

    Cosmetic improvements to Inspect's flow reporting

--- source/Runtime/SharpLabObjectExtensions.cs
+++ source/Runtime/SharpLabObjectExtensions.cs
@@ -6,12 +6,12 @@ public static class SharpLabObjectExtensions {
     // LinqPad/etc compatibility only
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static T Dump<T>(this T value) {
-        value.Inspect(title: "Dump");
+        Output.Write(new SimpleInspection("Dump", ValuePresenter.ToStringBuilder(value)));
         return value;
     }
 
-    public static void Inspect<T>(this T value, string title = "Inspect") {
-        Output.Write(new SimpleInspection(title, ValuePresenter.ToStringBuilder(value)));
+    public static void Inspect<T>(this T value, string title = null) {
+        Output.Write(new SimpleInspection(title ?? "Inspect", ValuePresenter.ToStringBuilder(value)));
 
         var lineNumber = Flow.GetLastReportedLineNumber();
         if (lineNumber != null)
