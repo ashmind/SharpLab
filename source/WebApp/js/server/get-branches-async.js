583fd4ca8cda73cbd1d71474fc8ffbed713056b3
Andrey Shchekin <ashmind@gmail.com>
2019-09-07 10:46:44 +1200

    Changed branch load to use blob data

--- source/WebApp/js/server/get-branches-async.js
+++ source/WebApp/js/server/get-branches-async.js
@@ -1,6 +1,25 @@
+const branchesUrl = (() => {
+    const mainUrl = 'https://slbs.azureedge.net/public/branches.json';
+    const edgeUrl = 'https://slbs.azureedge.net/public/branches-edge.json';
+
+    const host = window.location.host;
+    const override = (window.location.search.match(/[?&]branches=([^?&]+)/) || [])[1];
+
+    switch (host) {
+        case 'sharplab.io':
+            if (override)
+                throw new Error('Cannot override branch source on the main site (remove ?branches=).');
+            return mainUrl;
+        case 'edge.sharplab.io':
+            return override === 'main' ? mainUrl : edgeUrl;
+        default:
+            return { main: mainUrl, edge: edgeUrl }[override] || '!branches.json';
+    }
+})();
+
 export default async function getBranchesAsync() {
     try {
-        const branches = await (await fetch('!branches.json')).json();
+        const branches = await (await fetch(branchesUrl)).json();
         for (const branch of branches) {
             if (!branch.commits)
                 continue;
---
+++ source/WebApp/tests/server.get-branches-async.tests.js
@@ -0,0 +1,43 @@
+describe('get-branches-async', () => {
+    test.each([
+        // main domain (not overridable)
+        ['sharplab.io', '', 'https://slbs.azureedge.net/public/branches.json'],
+
+        // edge
+        ['edge.sharplab.io', '', 'https://slbs.azureedge.net/public/branches-edge.json'],
+        ['edge.sharplab.io', 'main', 'https://slbs.azureedge.net/public/branches.json'],
+        ['edge.sharplab.io', 'edge', 'https://slbs.azureedge.net/public/branches-edge.json'],
+
+        // local
+        ['sharplab.local', '', '!branches.json'],
+        ['sharplab.local', 'main', 'https://slbs.azureedge.net/public/branches.json'],
+        ['sharplab.local', 'edge', 'https://slbs.azureedge.net/public/branches-edge.json']
+    ])("fetches expected url for domain '%s' and query string '%s'", async (host, query, expected) => {
+        global.fetch = jest.fn();
+        delete window.location;
+        window.location = {
+            host,
+            search: query ? `?branches=${query}` : ''
+        };
+
+        jest.resetModules();
+        const getBranchesAsync = (await import('../js/server/get-branches-async.js')).default;
+
+        await getBranchesAsync();
+
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith(expected);
+    });
+});
+
+(() => {
+    let location;
+    beforeEach(() => {
+        location = window.location;
+    });
+
+    afterEach(() => {
+        delete global.fetch;
+        window.location = location;
+    });
+})();
