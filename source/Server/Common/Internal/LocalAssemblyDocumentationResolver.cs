4841ccaa269e13f8f00f11583e19c10b71772c00
Andrey Shchekin <ashmind@gmail.com>
2019-10-13 20:09:18 +1300

    [gh-397] Ensured core site deployment includes XML docs

--- source/Server/Common/Internal/LocalAssemblyDocumentationResolver.cs
+++ source/Server/Common/Internal/LocalAssemblyDocumentationResolver.cs
@@ -11,7 +11,7 @@ namespace SharpLab.Server.Common.Internal {
             if (fileName == "System.Private.CoreLib.xml")
                 fileName = "System.Runtime.xml";
 
-            var fullPath = Path.Combine(Path.GetDirectoryName(Current.AssemblyPath)!, fileName);
+            var fullPath = Path.Combine(Path.GetDirectoryName(Current.AssemblyPath)!, "xmldocs", fileName);
 
             if (!File.Exists(fullPath))
                 return null;
