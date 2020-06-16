4fab81660c8c579b99a094876f669501030346ed
Andrey Shchekin <ashmind@gmail.com>
2020-06-16 20:11:28 +1200

    master => main

--- #scripts/release.ps1
+++ #scripts/release.ps1
@@ -1,7 +1,7 @@
 Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop'
 
-cmd /c "git checkout master && git merge edge --ff-only && git push origin && git checkout edge"
+cmd /c "git checkout main && git merge edge --ff-only && git push origin && git checkout edge"
 if ($LastExitCode -ne 0) {
     throw "git failed with exit code $LastExitCode"
 }
