43efea64767caeceebe7c763807ad35069beb068
Andrey Shchekin <ashmind@gmail.com>
2020-07-20 21:56:20 +1200

    [gh-488] Fix sl setup

--- #scripts/setup.ps1
+++ #scripts/setup.ps1
@@ -1,76 +1,37 @@
 Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop'
 
-function New-SLSiteUnlessExists(
-    [string] $hostName,
-    [string] $relativePath,
-    [switch] $use32bit = $false
-) {
-    if (Get-IISSite | ? { $_.Name -eq $hostName }) {
-        return
-    }
-
-    $iisManager = Get-IISServerManager
-    $appPool = $iisManager.ApplicationPools.Add($hostName)
-    if ($use32bit) {
-        $appPool.Enable32BitAppOnWin64 = $true
-    }
-
-    $site = New-IISSite `
-        -Name $hostName `
-        -PhysicalPath (Resolve-Path $relativePath) `
-        -BindingInformation "*:80:$hostName" `
-        -Passthru
-
-    $site.Applications["/"].ApplicationPoolName = $hostName
-
-    Write-Host "Please add to etc/hosts: $hostName" -ForegroundColor Magenta
-}
-
-Write-Host "Fetching submodules"
+Write-Host "Fetching submodules" -ForegroundColor White
 git submodule update --recursive --init
 if ($LastExitCode -ne 0) {
     throw "git failed with exit code $LastExitCode"
 }
 
-Write-Host "Creating stub .env"
+Write-Host "Creating stub .env" -ForegroundColor White
 if (!(Test-Path './source/WebApp/.env')) {
     Copy-Item './source/WebApp/.env.template' './source/WebApp/.env'
 }
 
-Write-Host "Installing local tools"
+Write-Host "Installing local tools" -ForegroundColor White
 dotnet tool restore
 if ($LastExitCode -ne 0) {
     throw "dotnet failed with exit code $LastExitCode"
-} 
-
-Write-Host "Building and publishing"
-&"$PSScriptRoot\build.ps1" -Configuration Debug
-dotnet publish source/Server -c Debug --no-restore --no-build
-if ($LastExitCode -ne 0) {
-    throw "dotnet publish failed with exit code $LastExitCode"
 }
 
-Write-Host "Creating web sites (IIS)"
-$principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-IISCommitDelay
-    New-SLSiteUnlessExists "sharplab.local" -RelativePath "source/WebApp" -Use32Bit
-    New-SLSiteUnlessExists "sl-a-x64.sharplab.local" -RelativePath "source/Server.Owin"
-    Stop-IISCommitDelay
+Write-Host "Installing node modules" -ForegroundColor White
+Push-Location './source/WebApp'
+try {
+    npm ci   
+    if ($LastExitCode -ne 0) {
+        throw "npm ci failed with exit code $LastExitCode"
+    }
 }
-else {
-    Write-Warning "Skipping IIS setup. If you want to set up local sites, please run this script as an admin."
-}
-
-Write-Host "Generating stub !branches.json"
-if (!(Test-Path './source/WebApp/wwwroot/!branches.json')) {
-    $json = ConvertTo-Json @(
-        @{ id = 'x64'; name = 'x64'; url = 'http://sl-a-x64.sharplab.local'; group = 'Platforms'; kind = 'platform' },
-        @{ id = 'core-x64'; name = '.NET Core (x64)'; url = 'http://localhost:54100'; group = 'Platforms'; kind = 'platform' },
-        @{ id = 'core-x64-profiled'; name = '.NET Core (x64, Profiler)'; url = 'http://localhost:54200'; group = 'Platforms'; kind = 'platform' }
-    )
-    [IO.File]::WriteAllText("$(Get-Location)/source/WebApp/wwwroot/!branches.json", $json)
+finally {
+    Pop-Location
 }
 
-Write-Host "Done"
+Write-Host ""
+Write-Host "SharpLab setup done." -ForegroundColor White
+Write-Host "Run " -ForegroundColor White -NoNewLine
+Write-Host "dotnet tye run" -ForegroundColor Cyan -NoNewLine
+Write-Host " to start." -ForegroundColor White -NoNewLine
