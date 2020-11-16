dc4d9fa61806cab6190a4d7875ee939c52dbbb4b
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-11-16 12:02:33 +0200

    Fix an ApplicationInsights-related error.

--- source/Server/Integration/Azure/AzureModule.cs
+++ source/Server/Integration/Azure/AzureModule.cs
@@ -10,14 +10,14 @@ namespace SharpLab.Server.Azure {
     public class AzureModule : Module {
         protected override void Load(ContainerBuilder builder) {
             var instrumentationKey = Environment.GetEnvironmentVariable("SHARPLAB_TELEMETRY_KEY");
-            if (instrumentationKey == null)
+            var webAppName = Environment.GetEnvironmentVariable("SHARPLAB_WEBAPP_NAME");
+            if (instrumentationKey == null || webAppName == null)
                 return;
 
             var configuration = new TelemetryConfiguration { InstrumentationKey = instrumentationKey };
             builder.RegisterInstance(new TelemetryClient(configuration))
                    .AsSelf();
 
-            var webAppName = Environment.GetEnvironmentVariable("SHARPLAB_WEBAPP_NAME");
             builder.RegisterType<ApplicationInsightsMonitor>()
                    .As<IMonitor>()
                    .WithParameter("webAppName", webAppName)
