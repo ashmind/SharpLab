d387fdd5a2efb1bc5cfb57399c74b04005ccc045
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2021-01-03 12:43:15 +0200

    Add some more tokens in asm grammar builtins

--- source/WebApp/components/internal/codemirror/mode-asm.ts
+++ source/WebApp/components/internal/codemirror/mode-asm.ts
@@ -1,41 +1,12 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 import CodeMirror from 'codemirror';
+import instructionsRegex from './mode-asm-instructions';
 
 CodeMirror.defineMode('asm', () => {
     const grammar = {
-        builtin: new RegExp('^(?:' + [
-            /* spellchecker: disable */
-            'aa[adms]', 'adc', 'add', 'and', 'arpl',
-            'bound', 'bsf', 'bsr', 'bswap', 'bt', 'bt[crs]',
-            'call', 'cbw', 'cdq', 'cl[cdi]', 'clts', 'cmc', conditional('cmov'), 'cmp', 'cmps[bdw]', 'cmpxchg', 'cmpxchg8b', 'cpuid', 'cwd', 'cwde',
-            'daa', 'das', 'dec', 'div',
-            'enter', 'esc',
-            'hlt',
-            'idiv', 'imul', 'in', 'inc', 'ins', 'insd', 'int', 'int[13o]', 'invd', 'invlpg', 'iret', 'iret[df]',
-            conditional('j'), 'jecxz', 'jcxz', 'jmp',
-            'lahf', 'lar', 'lds', 'lea', 'leave', 'les', 'l[gil]dt', 'lfs', 'lgs', 'lmsw', 'loadall', 'lock', 'lods[bdw]', 'loop', 'loop[dw]?', 'loopn?[ez][dw]?', 'lsl', 'lss', 'ltr',
-            'mov', 'movs[bdw]', 'mov[sz]x', 'movsxd', 'mul',
-            'neg', 'nop', 'not',
-            'or', 'out', 'outsd?',
-            'pop', 'pop[af]d?', 'push', 'push[af]d?',
-            'rcl', 'rcr', 'rdmsr', 'rdpmc', 'rdtsc', 'rep', 'repn?[ez]', 'ret', 'retn', 'retf', 'rol', 'ror', 'rsm',
-            'sahf', 'sal', 'sar', 'sbb', 'scas[bdw]', conditional('set'), 's[gil]dt', 'shld?', 'shrd?', 'smsw', 'st[cdi]', 'stos[bdw]', 'str', 'sub', 'syscall', 'sysenter', 'sysexit', 'sysret',
-            'test',
-            'ud2',
-            'verr', 'verw',
-            'vmovdqu', 'vmovdqu8', 'vmovdqu16', 'vmovdqu32', 'vxorps',
-            'wait', 'wbinvd', 'wrmsr',
-            'xadd', 'xchg', 'xlat', 'xor', 'xorps'
-            /* spellchecker: enable */
-        ].join('|') + ')(?:$|\\s)')
+        builtin: instructionsRegex
     };
-
-    function conditional(mnemonic: string) {
-        /* spellchecker: disable */
-        return mnemonic + '(?:n?[abgl]e?|n?[ceosz]|np|p[eo]?)';
-        /* spellchecker: enable */
-    }
 
     return {
         startState() {
