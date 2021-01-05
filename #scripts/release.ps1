ef7f7a4a46c4015dd2dc263320434eeda656dab6
Andrey Shchekin <ashmind@gmail.com>
2021-01-05 18:18:55 +1300

    Remove remaining use of edge branch and AppVeyor

--- #scripts/release.ps1
+++
@@ -1,9 +0,0 @@
-Set-StrictMode -Version 2
-$ErrorActionPreference = 'Stop'
-
-cmd /c "git checkout main && git merge edge --ff-only && git push origin && git checkout edge"
-if ($LastExitCode -ne 0) {
-    throw "git failed with exit code $LastExitCode"
-}
-
-Write-Host Done
