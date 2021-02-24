63b029ee83cb45f6dad991495e1b66105069cd73
Andrey Shchekin <ashmind@gmail.com>
2021-02-25 00:16:01 +1300

    Update to latest Unbreakable

--- source/Tests/TestCode/Execution/Api.Expressions.Simple.cs
+++ source/Tests/TestCode/Execution/Api.Expressions.Simple.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Linq.Expressions;
 
 public static class Program {
     public static void Main() {
-        Expression<Func<int, int, string>> f = (a, b) => $"{a}{b}";
+        Expression<Func<int, int, int>> f = (a, b) => a + b;
     }
 }
