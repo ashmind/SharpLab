ddb009a6059b313e3d2e977182a477caa5407e9a
Andrey Shchekin <ashmind@gmail.com>
2021-05-25 21:57:04 +1200

    Remove session debug log

--- source/Container.Manager/Internal/ExecutionEndpoint.cs
+++ source/Container.Manager/Internal/ExecutionEndpoint.cs
@@ -11,12 +11,10 @@ namespace SharpLab.Container.Manager.Internal {
     public class ExecutionEndpoint {
         private readonly ExecutionHandler _handler;
         private readonly ExecutionEndpointSettings _settings;
-        private readonly SessionDebugLog _sessionDebugLog;
 
-        public ExecutionEndpoint(ExecutionHandler handler, ExecutionEndpointSettings settings, SessionDebugLog sessionDebugLog) {
+        public ExecutionEndpoint(ExecutionHandler handler, ExecutionEndpointSettings settings) {
             _handler = handler;
             _settings = settings;
-            _sessionDebugLog = sessionDebugLog;
         }
 
         public async Task ExecuteAsync(HttpContext context) {
@@ -36,11 +34,6 @@ namespace SharpLab.Container.Manager.Internal {
             timeoutSource.CancelAfter(10000);
             try {
                 var result = await _handler.ExecuteAsync(sessionId, memoryStream.ToArray(), timeoutSource.Token);
-                await context.Response.WriteAsync($"[Session Debug Log]\n", context.RequestAborted);
-                foreach (var message in _sessionDebugLog.GetAllLogMessages(sessionId)) {
-                    await context.Response.WriteAsync(message, context.RequestAborted);
-                }
-                await context.Response.WriteAsync($"[Session Debug Log]\n\n", context.RequestAborted);
 
                 var bytes = new byte[Encoding.UTF8.GetByteCount(result.Span)];
                 Encoding.UTF8.GetBytes(result.Span, bytes);
