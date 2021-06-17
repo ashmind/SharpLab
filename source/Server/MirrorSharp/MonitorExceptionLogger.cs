30fadb6b1f2296131b88f92513375b9b5d970d01
Andrey Shchekin <ashmind@gmail.com>
2021-06-17 20:36:08 +1200

    Cosmetic improvement

--- source/Server/MirrorSharp/MonitorExceptionLogger.cs
+++ source/Server/MirrorSharp/MonitorExceptionLogger.cs
@@ -14,7 +14,7 @@ namespace SharpLab.Server.MirrorSharp {
         public void LogException(Exception exception, IWorkSession session) {
             // Note/TODO: need to see if OperationCanceledException can be avoided
             // https://github.com/ashmind/SharpLab/issues/617
-            if (exception is WebSocketException || exception is OperationCanceledException)
+            if (exception is WebSocketException or OperationCanceledException)
                 return;
             _monitor.Exception(exception, session);
         }
