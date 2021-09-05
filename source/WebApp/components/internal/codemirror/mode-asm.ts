eecef4ae76a031ae6f6a2a72d4ac8bc3a3f8ce8c
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2021-09-05 16:36:29 +0300

    Add comment about pre-sorted array

--- source/WebApp/components/internal/codemirror/mode-asm.ts
+++ source/WebApp/components/internal/codemirror/mode-asm.ts
@@ -8,6 +8,9 @@ CodeMirror.defineMode('asm', () => {
         builtin: instructionsRegex
     };
     const specifiers = [ 'byte', 'dword', 'ptr', 'qword', 'short', 'tbyte', 'word' ];
+
+    // In order to match longest possible register name, this array is pre-sorted using naturalSort function:
+    // https://github.com/Bill4Time/javascript-natural-sort/blob/master/naturalSort.js
     const registers = [ 'rsp', 'rsi', 'rdx', 'rdi', 'rcx', 'rbx', 'rbp', 'rax', 'r15w', 'r15d', 'r15b', 'r15',
         'r14w', 'r14d', 'r14b', 'r14', 'r13w', 'r13d', 'r13b', 'r13', 'r12w', 'r12d', 'r12', 'r11w', 'r11d', 'r11b',
         'r11', 'r10w', 'r10d', 'r10b', 'r10', 'r9w', 'r9d', 'r9b', 'r9', 'r8w', 'r8d', 'r8b', 'r8', 'esp', 'esi',
