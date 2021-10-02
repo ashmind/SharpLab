b34f66be2e01945c57bacaa1258982c342a72bc1
Andrey Shchekin <ashmind@gmail.com>
2021-10-02 16:32:21 +1300

    [gh-805] Fix NullReferenceException when attempting to create a global override in IL

--- source/Tests/Decompilation/LanguageILTests.cs
+++ source/Tests/Decompilation/LanguageILTests.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 using SharpLab.Tests.Internal;
+using SharpLab.Server.Common;
+using System.Linq;
 
 namespace SharpLab.Tests.Decompilation {
     public class LanguageILTests {
@@ -24,6 +26,34 @@ namespace SharpLab.Tests.Decompilation {
             var decompiledText = result.ExtensionResult?.Trim();
             Assert.True(string.IsNullOrEmpty(errors), errors);
             code.AssertIsExpected(decompiledText, _output);
+        }
+
+        [Theory]
+        [InlineData("BaseM")]
+        [InlineData("Base::M")]
+        public async Task SlowUpdate_ReturnsErrorDiagnostic_ForMethodOverrideOutsideOfType(string baseMethod) {
+            // Arrange
+            var code = @"
+                .assembly _ {
+                }
+
+                .method hidebysig newslot virtual 
+	                instance void M() cil managed 
+                {
+	                .override method instance void " + baseMethod + @"()
+                    ret
+                }
+            ";
+            var driver = await TestDriverFactory.FromCode(code, LanguageNames.IL, LanguageNames.IL);
+
+            // Act
+            var result = await driver.SendSlowUpdateAsync<string>();
+
+            // Assert
+            Assert.Equal(
+                new[] { ("error", "IL", "Method 'M' is outside class scope and cannot be an override.") },
+                result.Diagnostics.Select(d => (d.Severity, d.Id, d.Message)).ToArray()
+            );
         }
     }
 }
