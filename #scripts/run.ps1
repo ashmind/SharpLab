b71b73759a127799d50c068ad63ede266982a713
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-11-01 12:10:29 +0000

    Added linux variant of scripts for building with codespaces.

--- #scripts/run.ps1
+++ #scripts/run.ps1
@@ -4,6 +4,11 @@ $ErrorActionPreference = 'Stop'
 $webappServerUrl = 'http://localhost:44100'
 
 Write-Host "Opening new window for initial wait" -ForegroundColor White
-Start-Process powershell -ArgumentList "-File `"$PSScriptRoot/run/wait.ps1`""
+
+if ($IsWindows) {
+    Start-Process PowerShell -ArgumentList "-File `"$PSScriptRoot/run/wait.ps1`""
+} else {
+    Start-Process pwsh -ArgumentList "-File `"$PSScriptRoot/run/wait.ps1`""
+}
 
 dotnet tye run --watch
