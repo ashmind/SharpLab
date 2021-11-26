5f54c06167f603e9b7e5042766ef013fa0bd3b4e
Fantoom <serge.vardanyan00@mail.ru>
2021-11-27 00:35:08 +0400

    Bracket moved to next line
    Before
    
    using System;
    public class C {
        public void M() {
        }
    }
    
    After
    
    using System;
    public class C 
    {
        public void M() 
        {
        }
    }

--- source/WebApp/ts/state/handlers/defaults.ts
+++ source/WebApp/ts/state/handlers/defaults.ts
@@ -12,7 +12,7 @@ const normalize = (code: string) => {
 };
 
 const code = asLookup({
-    [languages.csharp]: 'using System;\r\npublic class C {\r\n    public void M() {\r\n    }\r\n}',
+    [languages.csharp]: 'using System;\r\npublic class C \r\n{\r\n    public void M() \r\n    {\r\n    }\r\n}',
     [languages.vb]: 'Imports System\r\nPublic Class C\r\n    Public Sub M()\r\n    End Sub\r\nEnd Class',
     [languages.fsharp]: 'open System\r\ntype C() =\r\n    member _.M() = ()',
     [languages.il]: normalize(`
