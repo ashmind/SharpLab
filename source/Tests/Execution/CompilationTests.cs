ef102bead1fdb97a8725e99afc8b9be896173065
Jay Kim <jaykim@hotmail.co.nz>
2022-04-05 20:32:45 +1200

    [gh-320] - Remove test log

--- source/Tests/Execution/CompilationTests.cs
+++ source/Tests/Execution/CompilationTests.cs
@@ -2,16 +2,10 @@ using System.Threading.Tasks;
 using SharpLab.Tests.Execution.Internal;
 using SharpLab.Tests.Internal;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace SharpLab.Tests.Execution {
     [Collection(TestCollectionNames.Execution)]
     public class CompilationTests {
-
-        public CompilationTests(ITestOutputHelper output) {
-            TestAssemblyLog.Enable(output);
-        }
-
         [Theory]
         [InlineData("UnsafeKeyword.cs")]
         public async Task Compilation_IsIncludedInOutput(string codeFileName) {
