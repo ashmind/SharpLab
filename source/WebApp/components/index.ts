72345c8bf8910a03474eb4c14bb6b0f6fd84be5a
Andrey Shchekin <ashmind@gmail.com>
2022-04-24 19:23:52 +1200

    Remove remaining Vue VerifyView

--- source/WebApp/components/index.ts
+++ source/WebApp/components/index.ts
@@ -1,7 +1,6 @@
 import './app-code-edit';
 import './app-code-view';
 import './app-ast-view';
-import './app-verify-view';
 import './app-explain-view';
 import './app-output-view';
 import './app-gist-manager';
@@ -10,9 +9,11 @@ import './app-mobile-settings';
 import { WarningSection } from 'app/results/diagnostics/WarningSection';
 import { Diagnostic } from 'app/results/diagnostics/Diagnostic';
 import { Footer } from 'app/Footer';
+import { VerifyView } from 'app/results/VerifyView';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const reactComponents = {
+    'react-verify-view': VerifyView as any,
     'react-warnings-section': WarningSection as any,
     'react-diagnostic': Diagnostic as any,
     'react-footer': Footer as any
