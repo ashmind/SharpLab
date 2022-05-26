650329651b06a9ee092986c3cb83dd3e5058f495
Andrey Shchekin <ashmind@gmail.com>
2022-05-27 00:46:12 +1200

    Add stories for Diagnostic

--- source/WebApp/app/structure/results/Diagnostic.tsx
+++ source/WebApp/app/structure/results/Diagnostic.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
-import type { Diagnostic as DiagnosticItem, ServerError } from '../../ts/types/results';
+import React from 'react';
+import type { Diagnostic as DiagnosticItem, ServerError } from '../../shared/resultTypes';
 
 type Props = {
     data: DiagnosticItem|ServerError;
 };
 
-export const Diagnostic: FC<Props> = ({ data }) => {
+export const Diagnostic: React.FC<Props> = ({ data }) => {
     return <div className="diagnostic">
         {'id' in data && `${data.severity} ${data.id}: `}{data.message}
     </div>;
