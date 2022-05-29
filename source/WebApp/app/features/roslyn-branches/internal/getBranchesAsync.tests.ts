699fef6f57ff4beec5c4b4fc31e46d7a92eb525d
Andrey Shchekin <ashmind@gmail.com>
2022-05-29 14:55:34 +1200

    Fix tests

--- source/WebApp/app/features/roslyn-branches/internal/getBranchesAsync.tests.ts
+++ source/WebApp/app/features/roslyn-branches/internal/getBranchesAsync.tests.ts
@@ -25,7 +25,8 @@ describe('get-branches-async', () => {
 
         jest.resetModules();
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        // @ts-expect-error 'TODO: needs separate target setting for type check, but actually works'
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore 'TODO: needs separate target setting for type check, but actually works'
         const { getBranchesAsync } = (await import('./getBranchesAsync'));
 
         await getBranchesAsync();
