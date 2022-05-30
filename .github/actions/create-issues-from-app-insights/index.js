5f8200febb2ad8ba009df20dcb28c511c4aa11d8
Andrey Shchekin <ashmind@gmail.com>
2022-05-30 19:16:36 +1200

    [gh-1130] Fix script path

--- .github/actions/create-issues-from-app-insights/index.js
+++ .github/actions/create-issues-from-app-insights/index.js
@@ -1,7 +1,7 @@
 import exec from '@actions/exec';
 
 try {
-    await exec.exec('ts-node-esm index.ts');
+    await exec.exec('./node_modules/.bin/ts-node-esm index.ts');
 }
 catch (error) {
     console.error(error);
