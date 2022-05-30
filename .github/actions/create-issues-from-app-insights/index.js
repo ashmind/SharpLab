e8610e39a5e43bfb6b2e475b0b3ad3982f13f1ce
Andrey Shchekin <ashmind@gmail.com>
2022-05-30 19:34:21 +1200

    [gh-1130] Refix script path

--- .github/actions/create-issues-from-app-insights/index.js
+++ .github/actions/create-issues-from-app-insights/index.js
@@ -1,7 +1,10 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import exec from '@actions/exec';
 
 try {
-    await exec.exec('./node_modules/.bin/ts-node-esm index.ts');
+    const basePath = dirname(fileURLToPath(import.meta.url));
+    await exec.exec(`"${basePath}/node_modules/.bin/ts-node-esm"`, [`${basePath}/index.ts`]);
 }
 catch (error) {
     console.error(error);
