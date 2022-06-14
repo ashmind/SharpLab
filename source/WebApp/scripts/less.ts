8f61b048be7a7599c83fa60cf4203812ae514c42
Andrey Shchekin <ashmind@gmail.com>
2022-06-14 21:43:28 +1200

    Include fonts into WebApp deployment; fix diff output path

--- source/WebApp/scripts/less.ts
+++ source/WebApp/scripts/less.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import { task } from 'oldowan';
 import jetpack from 'fs-jetpack';
 import { inputRoot, outputVersionRoot } from './shared';
@@ -8,35 +9,57 @@ export const less = task('less', async () => {
     // @ts-expect-error (no typings)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const csso = (await import('postcss-csso')).default;
+    const postcssUrl = (await import('postcss-url')).default;
     const autoprefixer = (await import('autoprefixer')).default;
 
     const sourcePath = `${inputRoot}/less/app.less`;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const content = (await jetpack.readAsync(sourcePath))!;
-    let { css, map } = await lessRender.render(content, {
+    const initial = await lessRender.render(content, {
         filename: sourcePath,
         sourceMap: {
             sourceMapBasepath: inputRoot,
             outputSourceFiles: true
-        }
-    });
-    // @ts-expect-error (TODO: need to sort out 'map' type here)
-    ({ css, map } = await postcss([
+        },
+        rewriteUrls: 'local'
+    } as Less.Options);
+    const fonts = [] as Array<[from: string, to: string]>;
+    const { css, map, messages } = await postcss([
         autoprefixer,
+        postcssUrl([
+            {
+                filter: 'node_modules/@fontsource/**/files/*.*',
+                url: ({ url, absolutePath }) => {
+                    const targetPath = url.replace(
+                        /^.*@fontsource\/[^/]+\/files\/(.+)$/,
+                        './fonts/$1'
+                    );
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    fonts.push([absolutePath!, path.join(outputVersionRoot, targetPath)]);
+                    return targetPath;
+                }
+            }
+        ]),
         // no typings for csso
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         csso({ restructure: false })
-    ]).process(css, {
+    ]).process(initial.css, {
         from: sourcePath,
         map: {
             inline: false,
-            prev: map
+            prev: initial.map
         }
-    }));
+    });
+
+    for (const message of messages) {
+        const log = message.type === 'warning' ? 'warn' : 'log';
+        console[log](message.toString());
+    }
 
     const outputPath = `${outputVersionRoot}/app.min.css`;
     await Promise.all([
         jetpack.writeAsync(outputPath, css),
-        jetpack.writeAsync(outputPath + '.map', map)
+        jetpack.writeAsync(outputPath + '.map', JSON.stringify(map)),
+        ...fonts.map(([from, to]) => jetpack.copyAsync(from, to))
     ]);
 }, { watch: [`${inputRoot}/less/**/*.less`] });
