6c34e300af5377381bb9e885dd93a4560050bb7e
Andrey Shchekin <ashmind@gmail.com>
2022-11-07 21:53:40 +1300

    Update author links

--- source/WebApp/app/structure/Footer.tsx
+++ source/WebApp/app/structure/Footer.tsx
@@ -13,10 +13,10 @@ export const Footer: React.FC = () => {
         <MobileFontSizeSwitch />
         <DarkModeSwitch />
         <span className="footer-author-full">
-            Built by <a href="http://twitter.com/ashmind">Andrey Shchekin (@ashmind)</a> — see <a href="http://github.com/ashmind/SharpLab">SharpLab on GitHub</a>.
+            Built by <a href="https://linktr.ee/ashmind">Andrey Shchekin (@ashmind)</a> — see <a href="https://github.com/ashmind/SharpLab">SharpLab on GitHub</a>.
         </span>
         <span className="footer-author-mobile">
-            <a href="http://github.com/ashmind/SharpLab">SharpLab</a> by <a href="http://twitter.com/ashmind">Andrey Shchekin (@ashmind)</a>
+            <a href="https://github.com/ashmind/SharpLab">SharpLab</a> by <a href="https://linktr.ee/ashmind">Andrey Shchekin (@ashmind)</a>
         </span>
     </>;
 
