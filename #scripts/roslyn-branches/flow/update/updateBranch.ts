dee68c5b13d8ef751d7b5c73ee6d2970dc849c8e
Andrey Shchekin <ashmind@gmail.com>
2024-11-19 01:15:59 +1300

    [gh-1349] Update branch build to .NET 9

--- #scripts/roslyn-branches/flow/update/updateBranch.ts
+++ #scripts/roslyn-branches/flow/update/updateBranch.ts
@@ -242,7 +242,7 @@ async function buildSharpLab(roslynPackagesRoot: string) {
     });
 
     return {
-        publishRoot: `${branchSourceRoot}/Server/bin/Release/net8.0/publish`
+        publishRoot: `${branchSourceRoot}/Server/bin/Release/net9.0/publish`
     };
 }
 
